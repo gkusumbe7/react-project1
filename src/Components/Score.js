d38dd18a573100578753fb75a1496c52bfa037d7
gkusumbe7 <gkusumbe7@gmail.com>
2023-06-17 07:01:30 +0530

    added score css

--- src/Components/Score.js
+++ src/Components/Score.js
@@ -1,8 +1,19 @@
+import Navbar from "./Navbar";
 import "./Score.css";
 function Score(){
     return(
         <>
+        <Navbar/>
+        <div>
+            <h1>Score Board</h1>
         
+          <div className="score-card">
+            <h2>Team A </h2>
+          </div>
+          <div className="score-card">
+            <h2>Team B</h2>
+          </div>
+       </div> 
         </>
     );
 }
